038d5a5820473fff1b8b02450714b8a687f4c909
Lawrin Novitsky <lawrin.novitsky@mariadb.com>
2015-02-11 19:51:34 +0200

    SQLGetPrivateProfileString result is now checked to be >0(required for UnixODBC)

--- ma_dsn.c
+++ ma_dsn.c
@@ -144,7 +144,7 @@ my_bool MADB_ReadDSN(MADB_Dsn *Dsn, char *KeyValue, my_bool OverWrite)
     Dsn->DSNName= _strdup(Value);
     while (DsnKeys[i].DsnKey)
     {
-      if (SQLGetPrivateProfileString(Dsn->DSNName, DsnKeys[i].DsnKey, NULL, KeyVal, 1024, "ODBC.INI"))
+      if (SQLGetPrivateProfileString(Dsn->DSNName, DsnKeys[i].DsnKey, NULL, KeyVal, 1024, "ODBC.INI") > 0)
       {
         if (!MADB_DsnStoreValue(Dsn, DsnKeys[i].DsnOffset, KeyVal, DsnKeys[i].Type, OverWrite))
           return FALSE;
