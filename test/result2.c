d9c0ff41ab6aca07aefd7d5be47e2e1e60ccea8e
Lawrin Novitsky <lawrin.novitsky@mariadb.com>
2017-12-03 16:25:51 +0100

    Adding MariaDB 5.5 server to Travis tests. Skipping one tests with 5.5 servers(and MySQL <5.7), since it doesn't make sense there.

--- test/result2.c
+++ test/result2.c
@@ -1056,7 +1056,7 @@ ODBC_TEST(t_odbc77)
   EXPECT_STMT(Stmt, SQLFetch(Stmt), SQL_NO_DATA);
   CHECK_STMT_RC(Stmt, SQLFreeStmt(Stmt, SQL_CLOSE));
 
-  if (ServerNewerThan(Connection, 10, 2, 5))
+  if (ServerNotOlderThan(Connection, 10, 2, 5))
   {
     OK_SIMPLE_STMT(Stmt, "ANALYZE SELECT 1");
     CHECK_STMT_RC(Stmt, SQLFetch(Stmt));
