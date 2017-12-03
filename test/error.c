7d5f4fa28cbde5920c7d2f1040f12742eb9eae36
Lawrin Novitsky <lawrin.novitsky@mariadb.com>
2017-12-03 16:25:51 +0100

    Adding MariaDB 5.5 server to Travis tests. Skipping one tests with 5.5 servers(and MySQL <5.7), since it doesn't make sense there.

--- test/error.c
+++ test/error.c
@@ -615,6 +615,10 @@ ODBC_TEST(t_odbc94)
   SQLINTEGER  error;
   SQLSMALLINT len;
 
+  if (ServerNotOlderThan(Connection, 5, 7, 0) == FALSE)
+  {
+    skip("The test doesn't make sense in pre-10.0 servers, as the target query won't cause in 5.5(or pre-MySQL-5.7 the error it tests");
+  }
   sprintf((char *)conn, "DRIVER=%s;SERVER=%s;UID=%s;PASSWORD=%s;DATABASE=%s%s;",
     my_drivername, my_servername, my_uid, my_pwd, my_schema, ma_strport);
 
