2bc8a5b08395a83b660a6aae9978e26e58c92c04
Rasmus Johansson <razze@iki.fi>
2018-12-10 10:19:10 +0200

    Testing on Mac OSX
    OSX testing in travis
    
    Build instructions for OSX
    
    Separate for OSX since Docker isn't supported on OSX in Travis CI
    
    build on OS X
    
    Test on OS X
    
    Test on OS X
    
    Test on OS X
    
    Test on OS X
    
    Test on OS X
    
    Test on OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS/X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X
    
    Test OS X

---
+++ .travis/osx.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# set variables for Connector/ODBC
+export TEST_DRIVER="$PWD/libmaodbc.dylib"
+export TEST_DSN=madb_connstring_test
+export TEST_SERVER=localhost
+export TEST_SOCKET=
+export TEST_SCHEMA=odbc_test
+export TEST_UID=root
+export TEST_PASSWORD= 
+
+# upgrade openssl
+brew upgrade openssl
+# install unixodbc
+brew install libiodbc
+# install and start MariaDB Server
+brew install mariadb
+mysql.server start
+
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON -DWITH_IODBC=ON -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2q -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/1.0.2q/lib
+cmake --build . --config RelWithDebInfo
+###################################################################################################################
+# run test suite
+###################################################################################################################
+
+# set variables for odbc.ini and odbcinst.ini
+export ODBCINI="$PWD/test/odbc.ini"
+cat ${ODBCINI}
+export ODBCINSTINI="$PWD/test/odbcinst.ini"
+cat ${ODBCINSTINI}
+
+# check users of MariaDB and create test database
+mysql --version
+mysql -u root -e "SELECT user, host FROM mysql.user"
+mysql -u root -e "CREATE DATABASE odbc_test"
+mysql -u root -e "SHOW DATABASES"
+
+echo "Running tests"
+cd test
+ctest -V
