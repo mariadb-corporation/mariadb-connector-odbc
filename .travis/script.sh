5165a90b422f495e371f9bd0da27b2e17ec4c707
Lawrin Novitsky <lawrin.novitsky@mariadb.com>
2019-03-14 21:20:01 +0100

    The fix of the build in travis with latest C/C release

--- .travis/script.sh
+++ .travis/script.sh
@@ -63,7 +63,7 @@ export TEST_SCHEMA=odbc_test
 export TEST_UID=bob
 export TEST_PASSWORD= 
 
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL
 # In Travis we are interested in tests with latest C/C version, while for release we must use only latest release tag
 #git submodule update --remote
 cmake --build . --config RelWithDebInfo 
