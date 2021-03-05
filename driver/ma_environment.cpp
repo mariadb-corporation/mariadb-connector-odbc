19a0f4d3de1bb88163ee48c9c97a99c1a56253b3
Lawrin Novitsky <lawrin.novitsky@mariadb.com>
2021-03-05 18:51:46 +0100

    Just saving changes

--- driver/ma_environment.cpp
+++ driver/ma_environment.cpp
@@ -1,5 +1,5 @@
 /************************************************************************************
-   Copyright (C) 2013,2016 MariaDB Corporation AB
+   Copyright (C) 2013,2020 MariaDB Corporation AB
    
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
