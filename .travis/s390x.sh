3f04587615426be5dc59cc1760a83f4b83f3e647
Nayana Thorat <nthorat@us.ibm.com>
2021-06-09 11:08:54 +0530

    Create s390x.sh

---
+++ .travis/s390x.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -x
+set -e
+
+DEBIAN_FRONTEND=noninteractive sudo apt-get update
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y mariadb-server unixodbc-dev git cmake gcc libssl-dev tar curl libcurl4-openssl-dev libkrb5-dev 
+
+sudo service mysql start
+sudo ln -s /var/run/mysqld/mysqld.sock /tmp/mysql.sock
+sudo mysql -u root -e 'CREATE DATABASE IF NOT EXISTS test;'
+sudo mysql -u root -e "USE mysql; UPDATE user SET plugin='mysql_native_password' WHERE User='root'; FLUSH PRIVILEGES;"
+mysql --version
+
+# set variables for Connector/ODBC
+export TEST_DRIVER=maodbc_test
+export TEST_SCHEMA=test
+export TEST_DSN=maodbc_test
+export TEST_UID=root
+export TEST_PASSWORD=
+
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL -DODBC_LIB_DIR=/usr/lib/s390x-linux-gnu/
+cmake --build . --config RelWithDebInfo 
+
+###################################################################################################################
+# run test suite
+###################################################################################################################
+
+echo "Running tests"
+
+cd test
+export ODBCINI="$PWD/odbc.ini"
+export ODBCSYSINI=$PWD
+
+ctest -V
