a8b1c36d1b4c4038d98ff1ee4cb397c17a89f4a2
Nayana Thorat <nthorat@us.ibm.com>
2021-06-18 13:29:40 +0530

    Update s390x.sh

--- .travis/s390x.sh
+++ .travis/s390x.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 DEBIAN_FRONTEND=noninteractive sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y mariadb-server unixodbc-dev git cmake gcc libssl-dev tar curl libcurl4-openssl-dev libkrb5-dev 
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y mariadb-server-10.3 unixodbc-dev git cmake gcc libssl-dev tar curl libcurl4-openssl-dev libkrb5-dev 
 
 sudo service mysql start
 sudo ln -s /var/run/mysqld/mysqld.sock /tmp/mysql.sock
