b489f0b3648bc310b1de509817bd1ba2b98682b5
namrata-ibm <namrata.bhave@ibm.com>
2021-08-12 10:28:48 +0000

    Adding few fixes to the s390x Travis job

--- .travis/s390x.sh
+++ .travis/s390x.sh
@@ -4,8 +4,14 @@ set -x
 set -e
 
 DEBIAN_FRONTEND=noninteractive sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y mariadb-server unixodbc-dev git cmake gcc libssl-dev tar curl libcurl4-openssl-dev libkrb5-dev 
-
+# Remove existing MySQL to allow installation of MariaDB
+sudo service mysql stop
+sudo rm -rf /etc/mysql /var/lib/mysql /var/log/mysql
+sudo apt purge -y mysql-server mysql-client mysql-common
+sudo apt autoremove -y
+echo "/usr/sbin/mysqld { }" | sudo tee /etc/apparmor.d/usr.sbin.mysqld
+sudo apparmor_parser -v -R /etc/apparmor.d/usr.sbin.mysqld
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold mariadb-server unixodbc-dev git cmake gcc libssl-dev tar curl libcurl4-openssl-dev libkrb5-dev
 sudo service mysql start
 sudo ln -s /var/run/mysqld/mysqld.sock /tmp/mysql.sock
 sudo mysql -u root -e 'CREATE DATABASE IF NOT EXISTS test;'
