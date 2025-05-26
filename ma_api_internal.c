bc65180036a1493c7ed33106e46e1c19a6b9fee7
Lawrin Novitsky <lawrin.novitsky@mariadb.com>
2025-05-26 11:24:24 +0200

    ODBC-464 Amendment to the fix
    Made flag resetting synchronized in place where it is considered
    irrelevant. Even though it looks safe for this particular case, still
    race condition is possible and UB is not good.

--- ma_api_internal.c
+++ ma_api_internal.c
@@ -1,5 +1,5 @@
 /************************************************************************************
-   Copyright (C) 2020,2024 MariaDB Corporation plc
+   Copyright (C) 2020,2025 MariaDB Corporation plc
    
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
